bc6b733aeaf1dd279b325bd054e928f5aee65bc8
Eric QUERE <e_quere@yahoo.fr>
2021-05-28 12:34:27 +0200

    readme example

--- README.md
+++ README.md
@@ -8,17 +8,71 @@ Enter the Pkg REPL by pressing ```]``` from the Julia REPL. Then install the pac
 
 ## Usage
 
-Here we demonstrate the classic example of detecting linear and quadratic relationships with Hoeffding measure contrasted with Perason Correlation and Spearman's rank correlations.
+Here we demonstrate the classic example of detecting linear and quadratic relationships with Hoeffding measure contrasted with Perason Correlation and Spearman's rank correlation.
 
 ### Data generation
-'''julia 
+```julia 
 x = -2:0.1:2
 linear_f(x) = 2x ; quad_f(x) = x^2
 y_linear = linear_f.(x)
 y_quad = quad_f.(x)
-'''
+```
+
+which can be displayed as:
+```julia
+using Plots
+scatter(x, y_linear, label="linear")
+scatter!(x, y_quad, label="quadratic", legend=:bottomright)
+savefig("docs/linquad.png")
+```
+![scatterplot linear quadratic](docs/linquad.png)
+
+### Classic test
+
+```julia
+using StatsBase
+
+#Pearson Correlation
+@show(StatsBase.cor(x, y_linear))
+@show(StatsBase.cor(x, y_quad))
+#Spearman's rank correlation
+@show(StatsBase.corspearman(x, y_linear))
+@show(StatsBase.corspearman(x, y_quad))
+```
+which gives
+```
+StatsBase.cor(x, y_linear) = 1.0
+StatsBase.cor(x, y_quad) = 0.0
+StatsBase.corspearman(x, y_linear) = 1.0
+StatsBase.corspearman(x, y_quad) = 0.0
+```
+Both concurring that there is no correlation with the quadratic relationship.
+
+### Hoeffding D measure and D-test
+This can be contrasted with the Hoeffding measure.
+```julia
+@show(HoeffdingD.hoeffdingd(x, y_linear))
+@show(HoeffdingD.hoeffdingd(x, y_quad))
+```
+gives:
+```
+HoeffdingD.hoeffdingd(x, y_linear) = 1.0
+HoeffdingD.hoeffdingd(x, y_quad) = 0.2183712793468891
+```
+Which like Pearson Correlation and Spearman rank correlation indicates a perfect relationship for the linear relationship.
+For the quadratic relationship the D (dependence) value appears to be non-zero, but can the independence be ruled out?
+For that purpose we need to provide an α (between 0 and 1).
+```julia
+@show(HoeffdingD.hoeffdingd(x, y_linear, 0.05))
+@show(HoeffdingD.hoeffdingd(x, y_quad, 0.05))
+```
+Which gives:
+```
+HoeffdingD.hoeffdingd(x, y_linear, 0.05) = (1.0, true)
+HoeffdingD.hoeffdingd(x, y_quad, 0.05) = (0.2183712793468891, true)
+```
+The function now returns the D value with the result of the D-test of independence for the given α. Therefore indicating that in both case we can reject the H₀ hypothesis of independence.
 
 
-
-## Reference
-links
+## Questions
+Please post your questions or issues in the Issues tabs.
--- test/examples.jl
+++ test/examples.jl
@@ -1,91 +1,4 @@
-using Plots
-using StatsBase
-using BenchmarkTools
-using Distributions
-using HoeffdingD
-
-function compareD(interval=-√(0.99):0.05:√(0.99))
-    ρ = collect(interval) # -1:0.02:1
-    meanPearson = 0.
-    meanHoeffding = 0.
-
-    Pmeans = Vector{Float64}(undef, length(ρ))
-    Hmeans = Vector{Float64}(undef, length(ρ))
-    nb_sims = 10
-    for i in 1:length(ρ)
-        meanPearson = 0.
-        meanHoeffding = 0.
-        # meanDcor = 0.
-        meansP = Vector{Float64}(undef, nb_sims)
-        meansH = Vector{Float64}(undef, nb_sims)
-        for j in 1:nb_sims
-            z = rand(MvNormal([0., 0.], ρ[i]), 500)
-            a = view(z, 1, :)
-            b = view(z, 2, :)
-            meansP[j] = StatsBase.cor(a, b)
-            meansH[j] = first(HoeffdingD.hoeffdingd(a, b))
-        end
-        Pmeans[i] = mean(meansP)
-        Hmeans[i] = mean(meansH)
-    end
-
-    return Pmeans, Hmeans, ρ
-end
-
-Pmeans, Hmeans, ρ = compareD()
-
-plot(ρ, Pmeans)
-plot!(ρ, Hmeans)
-
-# N = 1000;
-# Mean = {1 2};
-# Cov = {2.4 3, 3 8.1};
-
-# x = RandNormal( N, Mean, Cov );
-# SampleMean = mean(x);
-# SampleCov = cov(x);
-# print SampleMean Mean, SampleCov Cov;
-
-covsigma = 
-@show(covsigma)
-
-MvNormal([0, 0], [-1, -1])
-
-# z1 = rand(MvNormal([0, 0], [-1 0 ; 0 -1]), 1000)
-z1 = rand(MvNormal([2, 1], [-1, -1]), 20000)
-# z1 = rand(MvNormal([2, 1], [2.4 1; 1 8.3 ]), 20000)
-a = view(z1, 1, :)
-b = view(z1, 2, :)
-@show(mean(a))
-@show(mean(b))
-@show(cov(a))
-@show(cov(b))
-@show(StatsBase.cor(a, b))
-@show(HoeffdingD.hoeffdingd(a, b))
-
-
-# The CORR Procedure
-
-# 6 Variables:	MSRP Invoice EngineSize Horsepower MPG_City MPG_Highway
-# Pearson Correlation Coefficients, N = 428
-#  	MSRP	Invoice	EngineSize	Horsepower	MPG_City	MPG_Highway
-# MSRP	1.00000	0.99913	0.57175	0.82695	-0.47502	-0.43962
-# Invoice	0.99913	1.00000	0.56450	0.82375	-0.47044	-0.43459
-# EngineSize	0.57175	0.56450	1.00000	0.78743	-0.70947	-0.71730
-# Horsepower	0.82695	0.82375	0.78743	1.00000	-0.67670	-0.64720
-# MPG_City	-0.47502	-0.47044	-0.70947	-0.67670	1.00000	0.94102
-# MPG_Highway	-0.43962	-0.43459	-0.71730	-0.64720	0.94102	1.00000
-# Hoeffding Dependence Coefficients, N = 428
-#  	MSRP	Invoice	EngineSize	Horsepower	MPG_City	MPG_Highway
-# MSRP	0.99945	0.93253	0.16067	0.35767	0.19844	0.14221
-# Invoice	0.93253	0.99995	0.15670	0.35327	0.19646	0.14201
-# EngineSize	0.16067	0.15670	0.88251	0.31255	0.35381	0.24217
-# Horsepower	0.35767	0.35327	0.31255	0.95970	0.27758	0.19187
-# MPG_City	0.19844	0.19646	0.35381	0.27758	0.76946	0.50466
-# MPG_Highway	0.14221	0.14201	0.24217	0.19187	0.50466	0.82463
-
-
-## quick example 
+# Readme example 
 
 x = -2:0.1:2
 linear_f(x) = 2x ; quad_f(x) = x^2
@@ -95,6 +8,7 @@ y_quad = quad_f.(x)
 using Plots
 scatter(x, y_linear, label="linear")
 scatter!(x, y_quad, label="quadratic", legend=:bottomright)
+savefig("docs/linquad.png")
 
 using StatsBase
 
@@ -104,6 +18,8 @@ using StatsBase
 @show(StatsBase.corspearman(x, y_quad))
 
 
-@show(HoeffdingD.hoeffdingd(x, y_linear, 0.01))
-@show(HoeffdingD.hoeffdingd(x, y_quad, 0.01))
+@show(HoeffdingD.hoeffdingd(x, y_linear))
+@show(HoeffdingD.hoeffdingd(x, y_quad))
+@show(HoeffdingD.hoeffdingd(x, y_linear, 0.05))
+@show(HoeffdingD.hoeffdingd(x, y_quad, 0.05))
 
